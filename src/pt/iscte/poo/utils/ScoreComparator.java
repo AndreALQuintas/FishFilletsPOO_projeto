f401d3a07b78f78efd7356ca9af10f2c602c0633
André Quintas <andre.a.l.quintas@gmail.com>
2025-12-03 20:28:41 +0000

    fixes do score comparator

---
+++ src/pt/iscte/poo/utils/ScoreComparator.java
@@ -0,0 +1,17 @@
+package pt.iscte.poo.utils;
+
+import java.util.Comparator;
+
+public class ScoreComparator implements Comparator<User> {
+
+
+	public int compare(User u1, User u2) {
+		if(u1.getMoveCount()==u2.getMoveCount()) {
+			if(u1.getTime().totalSeconds()-u2.getTime().totalSeconds()==0) {
+				Comparator<String> comp = String.CASE_INSENSITIVE_ORDER;
+				return comp.compare(u1.getName(), u2.getName());
+			}
+			return u1.getTime().totalSeconds()-u2.getTime().totalSeconds();
+		}return u1.getMoveCount()-u2.getMoveCount();
+	}
+}
